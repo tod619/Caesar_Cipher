10be60515e4131ee07812ed76c4b47f57de72af8
tod619 <trevorodwyer619@hotmail.com>
2023-05-28 21:27:15 +0100

    Create starter code for project in main.py file

--- main.py
+++ main.py
@@ -2,3 +2,9 @@
 # A simpler program to encryt and decrypt messages
 # 26/05/2023
 
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+            'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
