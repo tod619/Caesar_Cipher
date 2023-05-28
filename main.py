1f42542b8a200a57e67997c309dbe5f0b646fdeb
tod619 <trevorodwyer619@hotmail.com>
2023-05-28 21:45:19 +0100

    create encrypt function in main.py file

--- main.py
+++ main.py
@@ -8,3 +8,19 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
+
+
+def encrypt(plan_text, shift_amount):
+    """ Encrypts the user entered text by the given amounts of shift"""
+    cipher_text = ""
+    for letter in plan_text:
+        position = alphabet.index(letter)
+        new_position = position + shift_amount
+        new_letter = alphabet[new_position]
+        cipher_text += new_letter
+
+    print(f"the encoded text is: {cipher_text}")
+
+
+encrypt(text, shift)
+input("\n\nPress enter to exit.")
